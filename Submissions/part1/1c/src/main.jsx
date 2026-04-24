9d7dd91dbf8209d96024afa6d3069ec76e05e769
gladiatorcoder <shakir.mukhtar@oneguardian.in>
2026-04-24 12:42:02 +0530

    Counter function built in main.jsx using manual render

--- Submissions/part1/1c/src/main.jsx
+++ Submissions/part1/1c/src/main.jsx
@@ -2,8 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+let counter=0;
+
+const refresh = () => {
+  root.render(
+    <StrictMode>
+      <App counter={counter} />
+    </StrictMode>
+  )
+}
+
+setInterval(() => {
+  counter++;
+  refresh();
+}, 1000)
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
-    <App />
+    <App counter={counter}/>
   </StrictMode>,
 )
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
