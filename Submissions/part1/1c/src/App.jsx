51e3fb53d0a641e603f0d5ba9a0d100bfe1215ef
gladiatorcoder <shakir.mukhtar@oneguardian.in>
2026-04-24 12:01:27 +0530

    Age calculator component done.

---
+++ Submissions/part1/1c/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Hello = (props) => {
+  const bornYear = () => new Date().getFullYear() - props.age; 
+  
+  return(
+    <div>Hello {props.name}, you are {props.age} years old. You were born in {bornYear()}.</div>
+  )
+}
+
+const App = () => {
+  const name="Peter";
+  const age=10;
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name={name} age={age} />
+      <Hello name="Maya" age={24} />
+    </div>
+  )
+}
+
+export default App;
