0eb14098fb9d114a14f655ba3f78a2d7a1facaef
gladiatorcoder <shakir.mukhtar@oneguardian.in>
2026-04-30 19:09:55 +0530

    Calculations

--- Submissions/part1/1d/src/components/Stats.jsx
+++ Submissions/part1/1d/src/components/Stats.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import StatLine from './StatLine';
 
 const Stats = ({good, neutral, bad}) => {
+    const total = good+neutral+bad;
+    const positive = (good/(good+bad+neutral) * 100);
+    const avg = (good + (bad * -1))/3;
+
   if(good<1 && neutral<1 && bad<1){
     return(
       <div>
@@ -16,6 +20,9 @@ const Stats = ({good, neutral, bad}) => {
         <StatLine text="Good" score={good} />
         <StatLine text="Neutral" score={neutral} />
         <StatLine text="Bad" score={bad} />
+        {(avg) && <p>Average: {avg.toFixed(1)}</p>}
+        {(total) && <p>Total: {total}</p>}
+        {(positive) && <p>Positive: {positive.toFixed(1)}%</p>}
     </div>
   )
 }
