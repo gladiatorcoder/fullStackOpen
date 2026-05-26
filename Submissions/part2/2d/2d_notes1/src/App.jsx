fb49bd5665b231ee210030de71991c45f6122f72
gladiatorcoder <shakir.mukhtar@oneguardian.in>
2026-05-26 12:23:28 +0530

    Notes app to toggle individual note importance

--- Submissions/part2/2d/2d_notes1/src/App.jsx
+++ Submissions/part2/2d/2d_notes1/src/App.jsx
@@ -5,6 +5,9 @@ import axios from 'axios';
 
 const App = (props) => {
 
+  //Globals
+  const serverUrl = "http://localhost:3001/notes";
+
   //State
   const [notes, setNotes] = useState([]);
   const [filteredNotes, setFilteredNotes] = useState(props.notes);
@@ -13,7 +16,7 @@ const App = (props) => {
 
   //Effects
   useEffect(() => {
-    axios.get('http://localhost:3001/notes')
+    axios.get(serverUrl)
     .then(res => {
       if(res.data && res.data.length > 0) setNotes(res.data);
     })
@@ -22,6 +25,10 @@ const App = (props) => {
     })
   }, []);
 
+  // State variable based calculations
+  let importantNotes = notes.filter(note => note.important);
+
+  //Functions
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
@@ -30,12 +37,9 @@ const App = (props) => {
       // id: String(notes.length + 1),
     };
 
-    axios.post('http://localhost:3001/notes', noteObject)
+    axios.post(serverUrl, noteObject)
     .then(res => {
       setNotes(notes.concat(res.data));
-      if(res.data.important){
-        setFilteredNotes(notes.concat(res.data));
-      }
       setNewNote("");
     }).catch(err => {
       console.log("Error posting the new note: ", err);
@@ -43,7 +47,6 @@ const App = (props) => {
   }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value);
     setNewNote(event.target.value);
   }
 
@@ -58,15 +61,36 @@ const App = (props) => {
     }
   }
 
+  function toggleNoteImportance(e, id){
+    const noteToBeChanged = notes.find(note => note.id === id);
+    noteToBeChanged.important = !noteToBeChanged.important;
+    const workedNotes = notes.map(note => {
+      if(note.id===id){
+        return{
+          ...note,
+          important: !note.important
+        };
+      }
+      return note;
+    });
+
+    axios.put(`${serverUrl}/${id}`, noteToBeChanged)
+    .then(res => {
+      console.log(res.data);
+      setNotes(notes.map(note => note.id===id ? res.data : note));
+    })
+
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {showAll ? notes.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} importantNote={note.important} toggleNoteImportance={toggleNoteImportance} />
         )) :
-        filteredNotes.map(note => (
-          <Note key={note.id} note={note} />
+        importantNotes.map(note => (
+          <Note key={note.id} note={note} importantNote={note.important} toggleNoteImportance={toggleNoteImportance} />
         ))}
       </ul>
       <button onClick={() => handleShowAll(event)}>{showAll ? "Show important" : "Show all"}</button>
--- Submissions/part2/2d/2d_notes1/src/components/Note.jsx
+++ Submissions/part2/2d/2d_notes1/src/components/Note.jsx
@@ -1,5 +1,9 @@
-const Note = ({ note }) => {
-  return <li>{note.content}</li>
+const Note = ({ toggleNoteImportance, importantNote, note }) => {
+  return <li>
+    <span>{note.content}</span>&nbsp;&nbsp;
+    <button style={{cursor: "pointer"}} data-note-id={note.id} onClick={(e) => toggleNoteImportance(e, note.id)}>{importantNote ? "Not important" : "Important"}</button>
+  </li>
+
 }
 
-export default Note
+export default Note;
