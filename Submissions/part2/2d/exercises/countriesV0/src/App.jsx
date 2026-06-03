1087d898c009782423e53eb39e2976ff16ea5507
gladiatorcoder <shakir.mukhtar@oneguardian.in>
2026-06-03 20:51:42 +0530

    Added search results, notifications and single country view to countriesV0

--- Submissions/part2/2d/exercises/countriesV0/src/App.jsx
+++ Submissions/part2/2d/exercises/countriesV0/src/App.jsx
@@ -9,9 +9,8 @@ const App = () => {
   //State
   const [countries, setCountries] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-
-
-  //Computated values
+  const [searchResults, setSearchResults] = useState([]);
+  const [notification, setNotification] = useState("");
   
 
   //Effects
@@ -19,15 +18,38 @@ const App = () => {
     axios.get(`${baseURL}/api/all`)
     .then(res => {
       setCountries(res.data);
+      console.log("Fetched countries successfully: ", res.data);
     })
     .catch(err => console.log("Error fetching countries", err));
   }, []);
 
   useEffect(() => {
-    if(!searchTerm==="") return;
-    console.log("Searching the local copy of countries");
-    console.log("Searching for: ", searchTerm);
-    console.log("Local countries: ", countries);
+    if(searchTerm===""){
+      setSearchResults([]);
+      setNotification("");
+      return
+    };
+    if(countries.length < 1) return;
+    const searchResultsLocal = countries.filter(country => country.name.common.toLowerCase().includes(searchTerm.toLowerCase()));
+    console.log(searchResultsLocal);
+    if(searchResultsLocal.length > 10){
+      setSearchResults([]);
+      setNotification("Too many matches, specify another filter.");
+      let timer = setTimeout(() => {
+        setNotification("");
+      }, 3000);
+      return () => clearTimeout(timer);
+
+    }else if(searchResultsLocal.length===1){
+      setSearchResults(searchResultsLocal);
+      console.log(searchResults);
+      setNotification("");
+
+    }else{
+      setSearchResults(searchResultsLocal);
+      setNotification("");
+    }
+
   }, [searchTerm]);
 
 
@@ -42,6 +64,25 @@ const App = () => {
         <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
         <button type="submit">Search countries</button>
       </form>
+      {notification && <div>
+        {notification}  
+      </div>}
+      {searchResults && searchResults.length > 1 && <div className='searchResults'>
+        {searchResults.map(result => <div key={result.latlng[0]} className='searchResult'>
+          {result.name.common}
+        </div>)}
+      </div>}
+      {searchResults && searchResults.length === 1 && <div className="countryInfo">
+        <h4>{searchResults[0].name.common}</h4>
+        <div>Capital: {searchResults[0].capital[0]}</div>
+        <div>Area: {searchResults[0].area}</div>
+        <div>Languages
+          <ul>
+            {Object.values(searchResults[0].languages).map(language => <li key={language}>{language}</li>)}
+          </ul>
+        </div>
+        <img src={searchResults[0].flags.png} />
+      </div>}
     </section>
   )
 }
