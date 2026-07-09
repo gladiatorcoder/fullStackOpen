562dbb1793e84bfcad820adaa4597d1fd489c29f
gladiatorcoder <shakir.mukhtar@oneguardian.in>
2026-07-09 19:17:29 +0530

    Delete functionality added

--- Submissions/part3/3a/exercises/phoneBookV0/index.js
+++ Submissions/part3/3a/exercises/phoneBookV0/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const data = require("./db.json");
 const app = express();
 const PORT = 3001;
@@ -10,14 +10,6 @@ app.get("/", (req, res) => {
     res.send("PhoneBook");
 });
 
-// app.get("/api/persons", (req, res) => {
-//     if (data && data.length > 0) {
-//         res.status(200).json(data);
-//     } else {
-//         res.status(404).send("Data was not found!");
-//     }
-// });
-
 
 // Page endpoints
 
@@ -28,10 +20,38 @@ app.get("/api/persons", (req, res) => {
         for (let i = 0; i < data.length; i++) {
             html += `
                 <p>${data[i].name} - ${data[i].number}</p>
-                <a href=/api/persons/${data[i].id}>View contact</a>
+                <a href="/api/persons/${data[i].id}">View contact</a>
+                <button data-delete-id="${data[i].id}" class="deleteContact">
+                    Delete contact
+                </button>
             `;
         }
     }
+
+    html += `
+        <script>
+            const deleteBtns = document.querySelectorAll(".deleteContact");
+
+            for (let i = 0; i < deleteBtns.length; i++) {
+                deleteBtns[i].addEventListener("click", deleteContact);
+            }
+
+            function deleteContact(e) {
+                const deleteId = e.target.dataset.deleteId;
+
+                fetch(\`http://localhost:3001/api/persons/\${deleteId}\`, {
+                    method: "DELETE"
+                })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                })
+                .catch(err => {
+                    console.error("Delete contact failed. Error:", err);
+                });
+            }
+        </script>
+    `;
 
     if (data && data.length) {
         res.send(html);
@@ -40,26 +60,37 @@ app.get("/api/persons", (req, res) => {
     }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+    console.log("Deleting:", req.params.id);
+    res.json({ message: "Delete successful" });
+});
+
+
 app.get("/api/persons/:id", (req, res) => {
-    const person = data.filter(person => {
+    const person = data.find(person => {
         return person.id === req.params.id;
     });
+
     console.log(person);
+
     if (person) {
-        res.send(`Name: ${person[0].name}<br>Number: ${person[0].number}`);
+        res.send(`Name: ${person.name}<br>Number: ${person.number}`);
     } else {
         res.send("Person not found!");
     }
 });
 
+
 app.get("/info", (req, res) => {
     if (data && data.length > 0) {
         const now = new Date();
-        res.send(`Phonebook has info for ${data.length} people.<br><br>
+
+        res.send(`
+            Phonebook has info for ${data.length} people.<br><br>
             Date: ${now.toLocaleDateString()}<br>
             Day: ${now.toLocaleDateString(undefined, { weekday: "long" })}<br>
             Time: ${now.toLocaleTimeString()}<br>
-            Timezone: ${Intl.DateTimeFormat().resolvedOptions().timeZone}<br>
+            Timezone: ${Intl.DateTimeFormat().resolvedOptions().timeZone}
         `);
     } else {
         res.send("No information found!");
